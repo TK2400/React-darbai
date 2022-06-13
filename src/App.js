60f13fb29abe49c27ccd1f528459a6db07a29dc7
T.Kav <tomas@plienas.lt>
2022-06-13 21:46:56 +0300

    html komponentas navigation su dinamiskai gaunama klase per props ; primary ir secondary

--- src/App.js
+++ src/App.js
@@ -1,12 +1,47 @@
 import './App.css';
-import './components/button.css'
-
-import Button from './components/Button';
+import Navigation from './components/Navigation';
+import logo from './logo.svg';
 
 export default function App() {
   return (
-    <div>
-      <Button text="Click me"/>
+    <div className='app'>
+      <header className='header'>
+        <img src={logo} className='header__logo' alt='logo' />
+        <Navigation navType='primary' />
+        <div className='header__hero'>
+          Header image
+        </div>
+      </header>
+      <main>
+        <div className='cards'>
+          <article className='card'>
+            <h2 className='card__title'>About</h2>
+            <p className='card__paragraph'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut non nisl vel velit convallis accumsan ut sit amet est. Nullam sit amet fermentum magna. Duis finibus congue quam nec rhoncus. Ut id dignissim orci.</p>
+          </article>
+          <article className='card'>
+            <h2 className='card__title'>Company</h2>
+            <p className='card__paragraph'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut non nisl vel velit convallis accumsan ut sit amet est. Nullam sit amet fermentum magna. Duis finibus congue quam nec rhoncus. Ut id dignissim orci.</p>
+          </article>
+          <article className='card'>
+            <h2 className='card__title'>Services</h2>
+            <p className='card__paragraph'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut non nisl vel velit convallis accumsan ut sit amet est. Nullam sit amet fermentum magna. Duis finibus congue quam nec rhoncus. Ut id dignissim orci.</p>
+          </article>
+        </div>
+
+        <div className="content">
+          <article className='content__article'>
+            <section>
+              <h2 className='content__title'>Services</h2>
+              <p className='content__paragraph'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut non nisl vel velit convallis accumsan ut sit amet est. Nullam sit amet fermentum magna. Duis finibus congue quam nec rhoncus. Ut id dignissim orci.</p>
+            </section>
+            <section>
+              <h3 className='content__sub-header'>Sub Header</h3>
+              <p className='content__paragraph'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut non nisl vel velit convallis accumsan ut sit amet est. Nullam sit amet fermentum magna. Duis finibus congue quam nec rhoncus. Ut id dignissim orci.</p>
+            </section>
+          </article>
+          <Navigation navType='secondary' />
+        </div>
+      </main>
     </div>
   );
 }
--- src/components/Button.js
+++
@@ -1,7 +0,0 @@
-export default function Button(props) {
-    console.log(props)
-    return (
-        <button> {props.text}</button>
-    )
-}
-
